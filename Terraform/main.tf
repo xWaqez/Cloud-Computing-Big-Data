cbed62800c2a81939b379c2e1578e434d5c8baaf
xWaqez <philipp.engelmann25@gmail.com>
2025-07-02 11:11:19 +0200

    feat(terraform): update Terraform configuration and state files
    - Added `.terraform.lock.hcl` for dependency locking.
    - Updated provider files under `.terraform/providers/registry.terraform.io/kreuzwerker/docker/3.6.2/darwin_arm64/`.
    - Modified `main.tf` to reflect new configuration changes.
    - Removed unused files: `outputs.tf`, `variables.tf`, and `versions.tf`.
    - Added `terraform.tfstate` and `terraform.tfstate.backup` for state management.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.4.0"
+
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
@@ -11,6 +13,26 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+############################
+# VARIABLES
+############################
+
+variable "image_name" {
+  description = "Name und Tag deines lokalen Docker-Images"
+  type        = string
+  default     = "immutable-nginx:v1"
+}
+
+variable "host_port" {
+  description = "Port auf dem Host"
+  type        = number
+  default     = 8080
+}
+
+############################
+# RESOURCES
+############################
+
 resource "docker_image" "immutable_nginx" {
   name         = var.image_name
   keep_locally = true
@@ -18,7 +40,7 @@ resource "docker_image" "immutable_nginx" {
 
 resource "docker_container" "webserver" {
   name  = "immutable_webserver"
-  image = docker_image.immutable_nginx.latest
+  image = docker_image.immutable_nginx.name
 
   ports {
     internal = 80
@@ -26,10 +48,17 @@ resource "docker_container" "webserver" {
   }
 }
 
-output "container_id" {
-  value = docker_container.webserver.id
+############################
+# OUTPUTS
+############################
+
+output "container_name" {
+  value = docker_container.webserver.name
 }
 
 output "webserver_url" {
   value = "http://localhost:${var.host_port}"
+}
+output "image_name" {
+  value = docker_image.immutable_nginx.name
 }
